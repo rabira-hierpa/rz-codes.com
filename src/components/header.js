8af1607f099847825b374ae3c6f41120508d8e68
Rabra Hierpa <rabira.hierpa@gmail.com>
2021-03-21 18:28:00 +0300

    header and hero section

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,88 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import RzCodesLogo from "../images/logo.svg"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header className="lg:px-16 px-6 bg-white shadow-md flex flex-wrap items-center lg:py-0 py-2">
+    <div className="flex-1 flex justify-between items-center">
+      <Link to="/" className="flex text-lg font-semibold">
+        <img
+          src={RzCodesLogo}
+          width="50"
+          height="50"
+          className="p-2"
+          alt="Rz Codes Logo"
+        />
+        <div className="mt-3 text-red-600">Rz Codes</div>
+      </Link>
+    </div>
+    <label htmlFor="menu-toggle" className="pointer-cursor lg:hidden block">
+      <svg
+        className="fill-current text-gray-900"
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+      >
+        <title>menu</title>
+        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+      </svg>
+    </label>
+    <input className="hidden" type="checkbox" id="menu-toggle" />
+    <div className="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
+      <nav className="flex items-center ">
+        <ul className="lg:flex items-center text-red-600 space-x-4 text-gray-700 pt-4 lg:pt-0">
+          <li>
+            <Link
+              className="text-red-600 hover:pb-4 hover:border-b-4 hover:border-yellow-400"
+              to="/blogs"
+            >
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="text-red-600 hover:pb-4 hover:border-b-4 hover:border-yellow-400"
+              to="/projects"
+            >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="text-red-600 hover:pb-4 hover:border-b-4 hover:border-yellow-400"
+              to="/apps"
+            >
+              Apps
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="text-red-600 hover:pb-4 hover:border-b-4 hover:border-yellow-400"
+              to="/desings"
+            >
+              Designs
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="text-red-600 hover:pb-4 hover:border-b-4 hover:border-yellow-400"
+              to="/my-journey"
+            >
+              My Journey
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="text-red-600 hover:pb-4 hover:border-b-4 hover:border-yellow-400"
+              to="/about"
+            >
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   </header>
 )
---
+++ src/pages/landing/hero.js
@@ -0,0 +1,59 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+const Hero = () => {
+  const data = useStaticQuery(graphql`
+    query HeroQuery {
+      allFile(
+        filter: {
+          extension: { regex: "/(jpg)/" }
+          name: { eq: "rabira-hierpa" }
+        }
+      ) {
+        edges {
+          node {
+            id
+            name
+            childImageSharp {
+              fluid {
+                src
+                base64
+                tracedSVG
+                srcWebp
+                srcSetWebp
+                originalImg
+                originalName
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <div className="gird grid-cols-1 md:grid-cols-2">
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        <div className="rounded-full py-10 ">
+          <StaticImage
+            src="../../images/landing/rabira-hierpa-web.jpg"
+            alt="logo"
+            placeholder="blurred"
+          />
+        </div>
+        <div className="p-10">
+          <div className="font-extrabold text-6xl text-yellow-400">Selam!</div>
+          <div className="font-bold text-4xl pt-20 pb-10 text-red-500">
+            I'm Rabra Hierpa
+          </div>
+          <div className="font-bold text-2xl text-yellow-500">
+            FOSS Enthusiast,Full-stack Web Developer,GIS Expert & Graphics
+            Designer{" "}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
