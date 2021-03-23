61df99f5278296ef2be90aa984a070eee1a88f89
Rabra Hierpa <rabira.hierpa@gmail.com>
2021-03-23 12:30:31 +0300

    added hero section

--- src/pages/landing/hero.js
+++ src/pages/landing/hero.js
@@ -1,55 +1,55 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 const Hero = () => {
-  const data = useStaticQuery(graphql`
-    query HeroQuery {
-      allFile(
-        filter: {
-          extension: { regex: "/(jpg)/" }
-          name: { eq: "rabira-hierpa" }
-        }
-      ) {
-        edges {
-          node {
-            id
-            name
-            childImageSharp {
-              fluid {
-                src
-                base64
-                tracedSVG
-                srcWebp
-                srcSetWebp
-                originalImg
-                originalName
-              }
-            }
-          }
-        }
-      }
-    }
-  `)
+  //   const data = useStaticQuery(graphql`
+  //     query HeroQuery {
+  //       allFile(
+  //         filter: {
+  //           extension: { regex: "/(jpg)/" }
+  //           name: { eq: "rabira-hierpa" }
+  //         }
+  //       ) {
+  //         edges {
+  //           node {
+  //             id
+  //             name
+  //             childImageSharp {
+  //               fluid {
+  //                 src
+  //                 base64
+  //                 tracedSVG
+  //                 srcWebp
+  //                 srcSetWebp
+  //                 originalImg
+  //                 originalName
+  //               }
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `)
   return (
     <div className="gird grid-cols-1 md:grid-cols-2">
       <div className="grid grid-cols-1 md:grid-cols-2">
         <div className="rounded-full py-10 ">
           <StaticImage
-            src="../../images/landing/rabira-hierpa-web.jpg"
+            src={`src/images/landing/rabira-hierpa-web.jpg`}
             alt="logo"
             placeholder="blurred"
           />
         </div>
         <div className="p-10">
           <div className="font-extrabold text-6xl text-yellow-400">Selam!</div>
-          <div className="font-bold text-4xl pt-20 pb-10 text-red-500">
-            I'm Rabra Hierpa
+          <div className="font-bold text-4xl pt-20 pb-10 text-red-600">
+            I'm Rabra Hierpa ...
           </div>
           <div className="font-bold text-2xl text-yellow-500">
             FOSS Enthusiast,
             <span className="text-red-600">Full-stack Web Developer</span>,GIS
-            Expert & Graphics Designer{" "}
+            Expert & <span className="text-red-600">Graphics Designer</span>
           </div>
         </div>
       </div>
