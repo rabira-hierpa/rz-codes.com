b6736e4495c9b4a7e3f894a4890e063ac83a3aca
Rabra <r>
2021-05-09 23:28:36 +0300

    💡 comment idel code

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,20 +1,20 @@
 import React from "react"
 import Hero from "./hero"
-import Apps from "./apps"
+import Apps from "../apps"
 import "../../styles/global.css";
-import { StaticImage } from "gatsby-plugin-image";
+// import { StaticImage } from "gatsby-plugin-image";
 
 const landing = () => {
   return (
     <div>
       <Hero />
       <Apps />
-      <div className=" container md:flex md:items-center mx-auto shadow-2xl sm:shadow-none">
-        <StaticImage
+      <div className=" container md:flex md:items-center mx-auto shadow-2xl sm:shadow-none mb-5">
+        {/* <StaticImage
           alt="poster"
           className="absolute backdrop-filter z-0 left-0 w-full h-vw-50 sm:h-vw-40 md:h-80 lg:h-88 xl:h-96 2xl:h-112 object-cover"
           src="../../images/landing/rabira-hierpa-web.jpg"
-        />
+        /> */}
       </div>
     </div>
   )
