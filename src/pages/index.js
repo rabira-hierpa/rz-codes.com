ac6675d2c0204a1b7d997fa6b2f5e095fef95d1b
Rabra <r>
2021-05-09 23:28:36 +0300

    💡 comment idel code

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,10 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Landing />
-    <p>
+    {/* <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    </p> */}
   </Layout>
 )
 
