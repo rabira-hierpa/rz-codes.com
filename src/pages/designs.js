36e5da11306dadc45c00dede5dbac837f03d34e6
Rabra <r>
2021-05-09 23:26:31 +0300

    ➕  added new pages

---
+++ src/pages/designs.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Designs = () => {
+  return (
+    <Layout>
+      <div className="min-h-screen">
+        Here are my <span className="text-9xl text-red-600">Designs!</span>
+      </div>
+    </Layout>
+  )
+}
+
+export default Designs
---
+++ src/pages/my-journey.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const MyJourney = () => {
+  return (
+    <Layout>
+      <div className="min-h-screen">
+        Here are my <span className="text-9xl text-red-600">My Journey!</span>
+      </div>
+    </Layout>
+  )
+}
+
+export default MyJourney
