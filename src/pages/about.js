b21271c37c0b218fc83cb248a25fbd62a12256d7
Rabra <r>
2021-05-09 23:25:26 +0300

    ➕  added about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const About = () => {
+  return (
+    <Layout>
+      <div className="min-h-screen">
+        Here are my <span className="text-9xl text-red-600">About!</span>
+      </div>
+    </Layout>
+  )
+}
+export default About
