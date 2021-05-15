ed1768494fdfbd2b011a5236de20aab1fe066b89
Rabra <r>
2021-05-15 17:27:53 +0300

    🔍 added seo

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const About = () => {
   return (
     <Layout>
+      <SEO title="About"></SEO>
       <div className="min-h-screen">
         Here are my <span className="text-9xl text-red-600">About!</span>
       </div>
--- src/pages/apps.js
+++ src/pages/apps.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const Projects = () => {
   return (
     <Layout>
+      <SEO title="Apps"></SEO>
       <div className="min-h-screen">
         Here are my <span className="text-9xl text-red-600">Apps</span>
       </div>
