5c8704889a69f6537fc60eba6bf6624007e4de4c
Rabra Hierpa <rabira.hierpa@gmail.com>
2021-05-15 15:13:09 +0300

    added  design page layout + data

--- src/pages/designs.js
+++ src/pages/designs.js
@@ -1,11 +1,53 @@
 import React from "react"
 import Layout from "../components/layout"
-
+import { useStaticQuery, graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 const Designs = () => {
+  const DesignData = useStaticQuery(graphql`
+    query {
+      allDesignsJson {
+        nodes {
+          id
+          graphics_designs {
+            childImageSharp {
+              gatsbyImageData(
+                placeholder: BLURRED
+                transformOptions: { fit: COVER }
+                quality: 100
+              )
+            }
+          }
+        }
+      }
+    }
+  `)
   return (
     <Layout>
       <div className="min-h-screen">
-        Here are my <span className="text-9xl text-red-600">Designs!</span>
+        <span className="text-9xl text-red-600">Designs!</span>
+        <div className="pt-5">
+          <ResponsiveMasonry
+            columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
+          >
+            <Masonry>
+              {DesignData.allDesignsJson.nodes[0].graphics_designs.map(
+                ({ childImageSharp }, idx) => {
+                  return (
+                    <div key={idx} className="flex p-2  rounded-lg">
+                      <GatsbyImage
+                        key={idx}
+                        className="px-8 py-6 rounded-lg hover:shadow-2xl duration-300 cursor-pointer"
+                        image={childImageSharp.gatsbyImageData}
+                        alt="Rz-Design"
+                      />
+                    </div>
+                  )
+                }
+              )}
+            </Masonry>
+          </ResponsiveMasonry>
+        </div>
       </div>
     </Layout>
   )
