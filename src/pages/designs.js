dd95ebf81122a3023d03a644e7e1a111259e5d9a
Rabra Hierpa <rabira.hierpa@gmail.com>
2021-05-18 10:54:34 +0300

    🔨 added descripton the design page

--- src/pages/designs.js
+++ src/pages/designs.js
@@ -5,7 +5,6 @@ import { GatsbyImage } from "gatsby-plugin-image"
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 import SEO from "../components/seo"
 
-
 const Designs = () => {
   const DesignData = useStaticQuery(graphql`
     query {
@@ -29,7 +28,16 @@ const Designs = () => {
     <Layout>
       <SEO title="Designs"></SEO>
       <div className="min-h-screen">
-        <span className="text-9xl text-red-600">Designs!</span>
+        <div className="text-9xl text-red-600 mt-5">Designs!</div>
+        <p className="pt-5">
+          Here are some the designs I made while taking a Graphics Design Course
+          @ Berehana Selam Printing Technology college. All of the desings are
+          made using Adobe Photoshop,Illustrator and InDesign. Most of them are
+          magazine cover,flyers,banners, rollups and postcards
+        </p>
+        <p className="pt-5 text-blue-800 text-2xl  font-semibold">
+          Photoshop Designs
+        </p>
         <div className="pt-5">
           <ResponsiveMasonry
             columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
@@ -51,6 +59,18 @@ const Designs = () => {
               )}
             </Masonry>
           </ResponsiveMasonry>
+        </div>
+        <p className="pt-5 text-yellow-800 text-2xl  font-semibold">
+          Illustrator Designs
+        </p>
+        <div className="text-5xl text-center font-extrabold text-gray-500 p-10 ">
+          Coming Soon!
+        </div>
+        <p className="pt-5 text-red-800 text-2xl  font-semibold">
+          Indesign Designs
+        </p>
+        <div className="text-5xl text-center font-extrabold text-gray-500 p-10 ">
+          Coming Soon!
         </div>
       </div>
     </Layout>
