7468a0bd09ea0faab0ec9a6e1886392364cd6a25
Rabra Hierpa <rabira.hierpa@gmail.com>
2021-05-18 09:23:32 +0300

    🔍 added seo

--- src/pages/my-journey.js
+++ src/pages/my-journey.js
@@ -1,9 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
-
+import SEO from "../components/seo"
 const MyJourney = () => {
   return (
     <Layout>
+      <SEO title="My Journey"></SEO>
       <div className="min-h-screen">
         Here are my <span className="text-9xl text-red-600">My Journey!</span>
       </div>
