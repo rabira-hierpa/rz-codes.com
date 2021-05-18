bfa431c9d28fbe34f398383c143bb3e47a42ee06
Rabra Hierpa <rabira.hierpa@gmail.com>
2021-05-18 10:01:04 +0300

    🚀  deploy v0.2 and updated readme file

---
+++ public/component---src-pages-404-js-4ca0b7317d496f0d3a6d.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{9616:function(t,e,n){"use strict";n.r(e);var u=n(7294),a=n(8227),l=n(3751);e.default=function(){return u.createElement(a.Z,null,u.createElement(l.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-4ca0b7317d496f0d3a6d.js.map
---
+++ public/component---src-pages-about-js-da2b06d9ebad16147705.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[682],{5518:function(e,t,a){"use strict";a.r(t);var l=a(7294),n=a(8227),r=a(3751);t.default=function(){return l.createElement(n.Z,null,l.createElement(r.Z,{title:"About"}),l.createElement("div",{className:"min-h-screen"},l.createElement("span",{className:"text-9xl text-red-600"},"About!")))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-da2b06d9ebad16147705.js.map
