ef53e3cfc229336f649d5a30116f1ef154dd08fb
Rabra Hierpa <rabira.hierpa@gmail.com>
2021-05-18 09:23:32 +0300

    🔍 added seo

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
-
+import SEO from "../components/seo"
 const Projects = () => {
   return (
     <Layout>
+      <SEO title="Projects"></SEO>
       <div className="min-h-screen">
         Here are my <span className="text-9xl text-red-600">Projects</span>
       </div>
