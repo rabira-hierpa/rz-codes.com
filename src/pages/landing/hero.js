4a16b5c80cc795d77d9588e04f55ffb7caeb5a00
Rabra <rabira.hierpa@gmail.com>
2022-11-05 00:04:46 +0300

    ✨ feat: added typed animation

--- src/pages/landing/hero.js
+++ src/pages/landing/hero.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
+import { TypeAnimation } from "react-type-animation"
 
 const Hero = () => {
   return (
@@ -12,23 +13,41 @@ const Hero = () => {
       <div className="p-10">
         <div className="rounded-full flex items-center justify-center ">
           <StaticImage
+            loading="lazy"
             className="rounded-full border-2 border-grey-600 shadow-sm h-72 w-72"
-            src="../../images/landing/rabira-hierpa-web.jpg"
+            src="https://blog.rz-codes.com/wp-content/uploads/2022/11/rabira-hierpa-web.jpg"
             alt="rzcodes"
           />
         </div>
         <div className="text-center">
-        <div className=" font-extrabold text-6xl pt-5 text-yellow-600">
-          Hello !
-        </div>
+          <div className=" font-extrabold text-6xl pt-5 text-yellow-600">
+            Hello !
+          </div>
           <div className="font-bold text-4xl py-5 text-red-600">
-            I'm Rabra Hierpa ...
+          <TypeAnimation sequence={
+            [
+              "I'm Rabra Hierpa ...",
+              1000,
+              "A Software Engineer...",
+              2000,
+              "GIS Developer... ",
+              1000,
+              "and FOSS enthusiast!",
+              1000
+            ]
+          } 
+          wrapper="div"
+          cursor={true}
+          repeat={Infinity}
+          style={{fontSize: '2rem'}}
+          />
+           
           </div>
-          <div className="font-bold text-2xl text-yellow-600">
+          {/* <div className="font-bold text-2xl text-yellow-600">
             FOSS Enthusiast,
-            <span className="text-yellow-600">Full-stack Web Developer</span> & GIS
-           Developer 
-          </div>
+            <span className="text-yellow-600">Full-stack Web Developer</span> &
+            GIS Developer
+          </div> */}
         </div>
         <div className="flex justify-center space-x-5 pt-5">
           <a
