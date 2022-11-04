11b9af0a8bca1d2da73a02efec17af2a0a09c5bb
rabra <rabira.hierpa@gmail.com>
2022-11-04 12:33:17 +0300

    ✨ feat: updated image links to pull from WP-CMS

--- src/pages/landing/landing-designs.js
+++ src/pages/landing/landing-designs.js
@@ -9,15 +9,7 @@ const LandingDesign = () => {
       allDesignsJson {
         nodes {
           id
-          graphics_designs {
-            childImageSharp {
-              gatsbyImageData(
-                placeholder: BLURRED
-                transformOptions: { fit: COVER }
-                quality: 100
-              )
-            }
-          }
+          images
         }
       }
     }
@@ -29,23 +21,22 @@ const LandingDesign = () => {
       </div>
       <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
         <Masonry>
-          {DesignData.allDesignsJson.nodes[0].graphics_designs.map(
-            ({ childImageSharp }, idx) => {
-              return idx < 3 ? (
-                <div
-                  key={idx}
-                  className="flex  space-x-2 space-y-2 p-1 border-4 border-gray-100 rounded-lg"
-                >
-                  <GatsbyImage
-                    key={idx}
-                    className="px-8 py-6 rounded-lg hover:shadow-2xl duration-300 cursor-pointer"
-                    image={childImageSharp.gatsbyImageData}
-                    alt="Rz-Design"
-                  />
-                </div>
-              ) : null
-            }
-          )}
+          {DesignData.allDesignsJson.nodes[0].images.map((imageUrl, idx) => {
+            return idx < 3 ? (
+              <div
+                key={"img_" + idx}
+                className="flex  space-x-2 space-y-2 p-1 border-4 border-gray-100 rounded-lg"
+              >
+                <img
+                  key={"graphics_img" + idx}
+                  className=" rounded-lg hover:shadow-2xl duration-300 cursor-pointer"
+                  src={imageUrl}
+                  loading="lazy"
+                  alt="Rz-Design"
+                />
+              </div>
+            ) : null
+          })}
         </Masonry>
       </ResponsiveMasonry>
       <Link to="/designs">
