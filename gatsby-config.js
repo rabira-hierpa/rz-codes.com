d76031f4b269f231d1761c93b3b5974635f291f8
rabra <rabira.hierpa@gmail.com>
2023-03-08 17:58:51 +0300

    📈 feat: added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,12 @@ module.exports = {
         },
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+      },
+    }`gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
