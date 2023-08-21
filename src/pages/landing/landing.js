fe7a0b0823d14e7d731fdeffe563a24b947931f0
rz-codes <rabira.hierpa@gmail.com>
2023-08-22 00:02:47 +0300

    fix: changes card background to white (#39)
    * 🔒️ fix: added GA tracking ID env
    
    * 📈 feat: changed GA plugin
    
    * 📦 feat: added official GA gatsby plugin
    
    * ✏️ fix: fixed `trackingIds` typo
    
    * 🙈 git[ignore]: removed wp cache
    
    * 💄 style: added styles for blog archive and post
    
    * 👔 feat: added images for blog post
    
    * 🔖 version: upgraded gatsby-wordpress-source to v7.19
    
    * ✨ feat(about): add about page section data and  page style
    
    * 💄 style(blog-post): added style for paragraph tags
    
    * 🔧 chore(landing): adds `id` for key props
    
    * 📝 docs(Readme): adds guide for env vars
    
    * 🐛 fix: changed `pointer-cursor` to `cursor-pointer` for the label htmlFor="menu-toggle".
    
    * fix: changes card background to white

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -12,13 +12,13 @@ const landing = () => {
       <LandingApps />
       <LandingProjects />
       <LandingDesign />
-      <div className=" container md:flex md:items-center mx-auto shadow-2xl sm:shadow-none mb-5">
-        {/* <StaticImage
+      {/* <div className=" container md:flex md:items-center mx-auto shadow-2xl sm:shadow-none mb-5"> */}
+      {/* <StaticImage
           alt="poster"
           className="absolute backdrop-filter z-0 left-0 w-full h-vw-50 sm:h-vw-40 md:h-80 lg:h-88 xl:h-96 2xl:h-112 object-cover"
           src="../../images/landing/rabira-hierpa-web.jpg"
         /> */}
-      </div>
+      {/* </div> */}
     </div>
   )
 }
