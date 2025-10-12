ab1b32332ee9860897cc28d303271e5958a3636b
rabra <rabira.hierpa@gmail.com>
2025-10-12 13:57:29 +0400

    ⚙️ Wrap app with ThemeProvider for SSR
    - Add ThemeProvider to wrapRootElement for SSR
    - Prevents theme context errors during build
    - Ensures consistent theme handling

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,10 @@
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+import React from "react"
+import { ThemeProvider } from "./src/context/theme-context"
+
+// Wrap the entire app with ThemeProvider for SSR
+export const wrapRootElement = ({ element }) => {
+  return <ThemeProvider>{element}</ThemeProvider>
+}
